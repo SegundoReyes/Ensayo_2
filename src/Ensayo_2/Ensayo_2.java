cef28ae169d636e403cc1a9380fb37f43802a570
bboyc <bboyc@DESKTOP-N0RDKUL>
2024-08-25 21:10:20 -0500

    Netbeans ejemplo

---
+++ src/Ensayo_2/Ensayo_2.java
@@ -0,0 +1,17 @@
+
+package Ensayo_2;
+import java.util.Scanner;
+import java.lang.Math;
+import java.text.DecimalFormat;
+public class Ensayo_2 {   
+public static void main(String[]args){
+double radio,Areacirculo;
+Scanner lectura = new Scanner ( System.in);  
+DecimalFormat formato= new DecimalFormat("##.##");
+System.out.print("Ingrese el radio del circulo:");
+radio=lectura.nextDouble();
+ Areacirculo=Math.PI*Math.pow(radio,2); 
+ System.out.println("El area de circulo es:"+formato.format (Areacirculo));
+ 
+ }   
+}
